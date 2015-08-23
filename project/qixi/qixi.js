52433f532804144124dc09fdc1ad8eb86c4992dc
hayson <anyan_hu@163.com>
2015-08-23 22:56:38 +0800

    update web1

--- project/qixi/qixi.js
+++ project/qixi/qixi.js
@@ -6,13 +6,13 @@ $(function(){
         .then(function() {
             //走完第一段路页面开始滚动
             swipe.scrollTo(5000, 1);
+        }).then(function() {
             //走第二段路
-            //return boy.walkTo(5000, 0.5);
+            return boy.walkTo(5000, 0.5);
         }).then(function() {
-            //return boy.walkTo(2000, 0.6)
-        }).then(function() {
-            //第三次走路完成
-            //boy.setColoer('blue')
+            openDoor();
+        }).then(function(){
+            shutDoor();
         });
 
     $(".cloud:first").addClass('cloud1Anim');
@@ -20,5 +20,40 @@ $(function(){
     $("#sun").addClass('rotation');
 
     //云的大小自适应
-    $('.cloud').css('transform','scale(' + $(document).height()/1000 + ')')
+    $('.cloud').css('transform','scale(' + $(document).height()/1000 + ')');
+
+    function doorAction(left, right, time) {
+        var $door = $('.door');
+        var doorLeft = $('.door-left');
+        var doorRight = $('.door-right');
+        var defer = $.Deferred();
+        var count = 2;
+        // 等待开门完成
+        var complete = function() {
+            if (count == 1) {
+                defer.resolve();
+                return;
+            }
+            count--;
+        };
+        doorLeft.animate({
+            'left': left
+        }, time, complete);
+        
+        doorRight.animate({
+            'left': right
+        }, time, complete);
+        
+         return defer;
+    }
+
+    // 开门
+    function openDoor() {
+        return doorAction('-50%', '100%', 2000);
+    }
+
+    // 关门
+    function shutDoor() {
+        return doorAction('0%', '50%', 2000);
+    }
 })
